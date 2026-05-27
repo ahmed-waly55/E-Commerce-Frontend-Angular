a10b1e5701f708bd30a87b8ea356d45746785d17
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-05-27 11:30:52 +0300

    core: add layout & config route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './core/layout/auth-layout/auth-layout.component';
+import { LoginComponent } from './core/components/login/login.component';
+import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
+import { HomeComponent } from './feature/components/home/home.component';
+import { NotfoundComponent } from './feature/components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path:"", component:AuthLayoutComponent, children:[
+    {path:"login", component:LoginComponent, title:"Login"},
+    {path:"register", component:LoginComponent, title:"Register"},
+
+  ]},
+  {path:"", component:MainLayoutComponent, children:[
+    {path:"home", component:HomeComponent, title:"Home"},
+    {path:"brands", component:HomeComponent, title:"Brands"},
+    {path:"cart", component:HomeComponent, title:"Cart"},
+    {path:"products", component:HomeComponent, title:"Products"},
+    {path:"categories", component:HomeComponent, title:"Categories"},
+  ]},
+  {path:"**", component:NotfoundComponent, title:"Not Found 404 "},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './feature/components/home/home.component';
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
